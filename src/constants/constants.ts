5a73c341c5d2cd455847d598748a12c9e4dd30bb
choihyerln <choi.hyehyehihi@gmail.com>
2024-11-17 20:46:10 +0900

    :bug: Fix: meta 코드 추가

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,24 @@
+// bottomSheet
 export const DRAWER_BLEEDING = 56;
+
+// api
+export const META = {
+  title: "기프투게더: 크라우드 펀딩",
+  siteName: "giftogether | 기프투게더",
+  description: "친구들에게 받고싶은 선물을 공유해보세요.",
+  keywords: [
+    "기프투게더",
+    "giftogether",
+    "크라우드펀딩",
+    "펀딩",
+    "선물펀딩",
+    "선물",
+    "후원",
+    "기부",
+  ],
+  // url: "https://giftogether.co.kr/",
+  url: "http://localhost:3000",
+  googleVerification: "_사이트 등록 후 받은 검증 코드_",
+  naverVerification: "_사이트 등록 후 받은 검증 코드_",
+  ogImage: "/dummy/present.png",
+} as const;
