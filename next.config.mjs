99808f577e13c711707491c36fd3e51e8c9b9738
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-11-30 15:11:43 +0900

    :wrench: chore: `@svgr/webpack` 의존성 및 관련 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -28,6 +28,13 @@ const nextConfig = {
             },
         ],
     },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"],
+        });
+        return config;
+    },
 };
 
 export default withVanillaExtract(nextConfig);
