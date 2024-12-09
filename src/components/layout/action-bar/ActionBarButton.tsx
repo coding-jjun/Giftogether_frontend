6154ea1297770913604bb24e04edbf3b58e46609
choihyerln <choi.hyehyehihi@gmail.com>
2024-12-10 00:14:29 +0900

    :lipstick: Design: 액션바 버튼 뷰포트 너비로 수정

--- src/components/layout/action-bar/ActionBarButton.tsx
+++ src/components/layout/action-bar/ActionBarButton.tsx
@@ -4,8 +4,8 @@ import { LoadingButton } from "@mui/lab";
 export const ActionBarButton = styled(LoadingButton)(() => ({
   width: "100%",
   height: "100%",
-  padding: 7,
-  fontSize: 17,
+  padding: "2vw",
+  fontSize: "5vw",
   letterSpacing: 2,
   borderRadius: 13,
   boxShadow: "none",
