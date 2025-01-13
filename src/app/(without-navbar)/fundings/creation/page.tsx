e2bc60658d85bee6e5c603aa8bc400e41ad21e24
choihyerln <choi.hyehyehihi@gmail.com>
2025-01-13 13:48:35 +0900

    🌱 Refactor: 불필요한 코드 제거 + gift 카드 1개 열어놓기

--- src/app/(without-navbar)/fundings/creation/page.tsx
+++ src/app/(without-navbar)/fundings/creation/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { Box, CssBaseline, Grid } from "@mui/material";
 import { DetailActionBar } from "@/components/layout/action-bar";
@@ -43,26 +43,11 @@ export default function FundingCreationPage() {
     null,
   );
 
-  const methods = useForm<FundingForm>();
-
-  const [gifts, setGifts] = useState<GiftDto[]>(getInitialGifts);
-
-  useEffect(() => {
-    const currentValues = methods.getValues("gifts"); // 현재 폼의 값
-
-    const mergedValues = gifts.map((gift, index) => {
-      const existGift = currentValues[index]; // 현재 폼에 입력된 값이 있는지 확인
-      return {
-        ...gift,
-        giftOpt: existGift?.giftOpt || gift.giftOpt, // 입력값 있으면 유지
-        giftUrl: existGift?.giftUrl || gift.giftUrl,
-        giftCont: existGift?.giftCont || gift.giftCont,
-      };
-    });
-
-    // gifts 배열이 바뀔 때만 폼을 업데이트하고, 폼이 채워져있는건 그대로 유지
-    methods.setValue("gifts", mergedValues);
-  }, [gifts, methods]);
+  const methods = useForm<FundingForm>({
+    defaultValues: {
+      gifts: getInitialGifts(),
+    },
+  });
 
   const { mutate } = useFundingCreateQuery();
 
